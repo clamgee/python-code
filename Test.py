c92b24fa4282feea76ccb8cceb0fa7603af38855
clamgee <clam.ke@gmail.com>
2018-09-26 05:48:46 +0800

    9/25 night

--- Test.py
+++ Test.py
@@ -1,23 +1,17 @@
-class t1():
-    def a():
-        a=55
-        b=45
-        x=function(a,b)
-        return x
-
-    def b():
-        b=100
-        return b
+import datetime
+import time
+import numpy as np
+import pandas as pd
+class Transfor():
+    def __init__(self,type,name):
+        self.type=type
+        self.name=name
+        self.tickpd=pd.DataFrame(columns=['date','time','bid','ask','close','volume'])
+    
+    def TicktoK(date,time,bid,ask,close,volume):
         
-    def c(x):
-        c=x**2
-        return c
-
-def function(a,b):
-    x=a*b
-    return x
 
 if __name__ == '__main__' :
-    print(t1.a())
-    print(t1.b())
-    print(t1.c(5))
+    ss=t1()
+    print(ss.a)
+    print(ss.b)
