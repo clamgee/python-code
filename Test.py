10112028748cea16ff6972d36980ccb75d6c8256
clamgee <clam.ke@gmail.com>
2018-10-12 07:02:17 +0800

    10/11

--- Test.py
+++ Test.py
@@ -1,32 +1,35 @@
-import numpy as np
-import pandas as pd
-import datetime
-import time
-import matplotlib
-import matplotlib.dates as mdates
-import matplotlib.pyplot as plt
-from mpl_finance import candlestick_ohlc
-import csv
+# import numpy as np
+# import pandas as pd
+# import datetime
+# import time
+# import matplotlib
+# import matplotlib.dates as mdates
+# import matplotlib.pyplot as plt
+# from mpl_finance import candlestick_ohlc
+# import csv
 
-csvpf=pd.DataFrame(columns=['ndatetime','open','high','low','close','volume'])
-print('DataFrame大小: ',csvpf.shape[0])
-csvpf[['open','high','low','close','volume']]=csvpf[['open','high','low','close','volume']].astype(int)
-with open('data.csv',mode='r',newline='') as file:
-    rows=csv.reader(file)
-    for row in rows:
-        ndatetime=datetime.datetime.strptime(row[0],'%Y-%m-%d %H:%M:%S.%f')
-        newlist=[ndatetime,int(row[1]),int(row[2]),int(row[3]),int(row[4]),int(row[5])]
-        csvpf.loc[ndatetime]=newlist
+# csvpf=pd.DataFrame(columns=['ndatetime','open','high','low','close','volume'])
+# print('DataFrame大小: ',csvpf.shape[0])
+# csvpf[['open','high','low','close','volume']]=csvpf[['open','high','low','close','volume']].astype(int)
+# with open('data.csv',mode='r',newline='') as file:
+#     rows=csv.reader(file)
+#     for row in rows:
+#         ndatetime=datetime.datetime.strptime(row[0],'%Y-%m-%d %H:%M:%S.%f')
+#         newlist=[ndatetime,int(row[1]),int(row[2]),int(row[3]),int(row[4]),int(row[5])]
+#         csvpf.loc[ndatetime]=newlist
 
-# print(csvpf)
-csvpf= csvpf.reset_index(drop=True)
-# print(csvpf)
-csvpf['ndatetime']=csvpf['ndatetime'].map(mdates.date2num)
-# print(csvpf)
-ohlc=csvpf[['ndatetime','open','high','low','close']]
-print(ohlc)
-# fig = plt.figure()
-ax1 = plt.subplot2grid((6,1),(0,0),rowspan=5,colspan=1)
-ax1.xaxis_date()
-candlestick_ohlc(ax1,ohlc.values,width=0.02,colorup='r',colordown='g')
-plt.show()
+# # print(csvpf)
+# csvpf= csvpf.reset_index(drop=True)
+# # print(csvpf)
+# csvpf['ndatetime']=csvpf['ndatetime'].map(mdates.date2num)
+# # print(csvpf)
+# ohlc=csvpf[['ndatetime','open','high','low','close']]
+# print(ohlc)
+# # fig = plt.figure()
+# ax1 = plt.subplot2grid((6,1),(0,0),rowspan=5,colspan=1)
+# ax1.xaxis_date()
+# candlestick_ohlc(ax1,ohlc.values,width=0.02,colorup='r',colordown='g')
+# plt.show()
+
+import comtypes.client as cc
+cc.GetModule(r'C:\Users\Clam\Documents\CapitalAPI\元件\x64\SKCOM.dll')
