2d825d06e6c96969bb380e312da62ab539742270
clamgee <clam.ke@gmail.com>
2019-05-16 11:08:28 +0800

    5/16

--- SKCOMInital.py
+++ SKCOMInital.py
@@ -1,31 +1,53 @@
-import comtypes.client as cc
-import shutil
+import sys
 import os
-from tkinter import filedialog
-try:
-    def GetDesktopPath():
+from PyQt5.uic import loadUi
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QApplication,QDialog,QFileDialog
+
+class UpdateSKCOM(QDialog):
+    def __init__(self):
+        super(UpdateSKCOM,self).__init__()
+        loadUi(r'APIver1/UI/SKCOMupdate.ui',self)
+        self.openSKCOMdirbtn.clicked.connect(self.openModulepath_click)
+
+    def openModulepath_click(self):
+        orgpath=self.GetDesktopPath()
+        ModulePath=QFileDialog.getExistingDirectory(self,'選擇SKCOM模組路徑',orgpath)
+        self.SKCOMdirpath.setText(ModulePath)
+    def GetDesktopPath(self):
         return os.path.join(os.path.expanduser('~'), r'AppData\Local\Programs\Python\Python37\Lib\site-packages\comtypes\gen')
 
-    folder=GetDesktopPath()
-    if os.path.exists(folder):
-        print(os.path.exists(folder))
-        print(folder)
-        shutil.rmtree(folder)
-        os.mkdir(folder)
-    elif not os.path.exists(folder) :
-        os.mkdir(folder)
-    else :
-        print('未安裝!!')
-    # cc.GetModule(r'./PythonExample/x64/SKCOM.dll')
-    fname = os.path.abspath(r'PythonExample\x64')
-    fname = filedialog.askopenfilename(title=u'選擇SKCOM.dll',initialdir=(os.path.expanduser(fname)))
-    print(fname)
-    if os.path.exists(fname):
-        cc.GetModule(fname)
-        print('完成更新!!')
-    else:
-        print('更新失敗!!')
-        os._exit(0)
-except OSError as e:
-    print('錯誤訊息: ',e)
+App=QApplication(sys.argv)
+Widget=UpdateSKCOM()
+Widget.show()
+sys.exit(App.exec_())
+# from PyQt5.QtCore import pyqtSlot
+# from PyQt5.QtWidgets import QApplication,QDialog
+
+# try:
+#     def GetDesktopPath():
+#         return os.path.join(os.path.expanduser('~'), r'AppData\Local\Programs\Python\Python37\Lib\site-packages\comtypes\gen')
+
+#     folder=GetDesktopPath()
+#     if os.path.exists(folder):
+#         print(os.path.exists(folder))
+#         print(folder)
+#         shutil.rmtree(folder)
+#         os.mkdir(folder)
+#     elif not os.path.exists(folder) :
+#         os.mkdir(folder)
+#     else :
+#         print('未安裝!!')
+#     # cc.GetModule(r'./PythonExample/x64/SKCOM.dll')
+#     fname = os.path.abspath(r'PythonExample\x64')
+#     fname = filedialog.askopenfilename(title=u'選擇SKCOM.dll',initialdir=(os.path.expanduser(fname)))
+#     print(fname)
+#     if os.path.exists(fname):
+#         cc.GetModule(fname)
+#         print('完成更新!!')
+#     else:
+#         print('更新失敗!!')
+#         os._exit(0)
+# except OSError as e:
+#     print('錯誤訊息: ',e)
     
