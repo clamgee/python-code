613db7414ac29b9e7a15250419a4a47e037a9d6f
clamgee <clam.ke@gmail.com>
2019-07-21 05:38:45 +0800

    7/21

--- APIver1/Main.py
+++ APIver1/Main.py
@@ -267,17 +267,20 @@ class SKReplyLibEvent:
         nErrorStr=skC.SKCenterLib_GetReturnCodeMessage(nErrorCode)
         print('連線失敗: ',bstrUserID,nErrorStr)
     def OnComplete(self,bstrUserID):
+        print(SKMain.replypd)
         SKMain.ReplyCRpdMode.setdata(SKMain.replypd)
+        SKMain.Reply_TBW.setModel(SKMain.ReplyCRpdMode)
         print('回報完成: ',bstrUserID)
     def OnNewData(self,bstrUserID,bstrData):
         Line=bstrData.split(',')
-        i=0
-        for row in Line :
-            print(i,',',row)
-            i+=1
+        # i=0
+        # for row in Line :
+        #     print(i,',',row)
+        #     i+=1
         tmplist=[[Line[8],Line[6],Line[11],Line[20],Line[2],Line[20],Line[20],Line[6],Line[6],Line[6],Line[0],Line[10],Line[23],Line[24],Line[24]]]
-        SKMain.replypd.append(pd.DataFrame(tmplist,columns=['商品名稱','買賣','委託價格','委託口數','委託狀態','成交口數','取消口數','條件','倉位','當沖','委託序號','委託書號','委託日期','委託時間','交易時段']),ignore_index=True)
-        
+        print(tmplist)
+        SKMain.replypd=SKMain.replypd.append(pd.DataFrame(tmplist,columns=['商品名稱','買賣','委託價格','委託口數','委託狀態','成交口數','取消口數','條件','倉位','當沖','委託序號','委託書號','委託日期','委託時間','交易時段']),ignore_index=True)
+        print('Test:',SKMain.replypd)
     
     def OnSmartData(self,bstrUserID,bstrData):
         print(bstrData,'智動回報:',bstrData)
