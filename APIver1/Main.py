d149cc84607fdba1f8273ca43978d1ed1714097c
clamgee <clam.ke@gmail.com>
2019-08-21 05:01:04 +0800

    8/21

--- APIver1/Main.py
+++ APIver1/Main.py
@@ -252,7 +252,10 @@ class SKMainWindow(QMainWindow): #主視窗
         # 填入完整帳號
         self.fOrder.bstrFullAccount =  self.Future_Acc_CBox.currentText()
         # 填入期權代號
-        self.fOrder.bstrStockNo = self.commodityline.text()
+        if self.commodityline.text()=='TX00':
+            self.fOrder.bstrStockNo='MTX00'
+        else:
+            self.fOrder.bstrStockNo = self.commodityline.text()
         # 買賣別
         if self.trade_act!=-1:
             self.fOrder.sBuySell = self.trade_act
@@ -280,7 +283,8 @@ class SKMainWindow(QMainWindow): #主視窗
         self.fOrder.sReserved = 0
         # bstrMessage=''
         m_nCode=skO.SendFutureOrder(self.SKID,False,self.fOrder)
-        print(skC.SKCenterLib_GetReturnCodeMessage(m_nCode))
+        print(m_nCode,type(m_nCode))
+        # print(skC.SKCenterLib_GetReturnCodeMessage(m_nCode))
         # self.SKMessage.textBrowser.append(str(skC.SKCenterLib_GetReturnCodeMessage(m_nCode)))
 
 
@@ -407,14 +411,16 @@ class SKReplyLibEvent:
         nErrorStr=skC.SKCenterLib_GetReturnCodeMessage(nErrorCode)
         print('連線失敗: ',bstrUserID,nErrorStr)
     def OnComplete(self,bstrUserID):
-        # print(SKMain.replypd)
-        SKMain.ReplyCRpdMode.setdata(SKMain.replypd)
-        SKMain.Reply_TBW.setModel(SKMain.ReplyCRpdMode)
+        # SKMain.ReplyCRpdMode.setdata(SKMain.replypd)
+        # SKMain.Reply_TBW.setModel(SKMain.ReplyCRpdMode)
         print('回報完成: ',bstrUserID)
     def OnNewData(self,bstrUserID,bstrData):
         Line=bstrData.split(',')
         tmplist=[[Line[8],Line[6],Line[11],Line[20],Line[2],Line[20],Line[20],Line[6],Line[6],Line[6],Line[0],Line[10],Line[23],Line[24],Line[24]]]
         SKMain.replypd=SKMain.replypd.append(pd.DataFrame(tmplist,columns=['商品名稱','買賣','委託價格','委託口數','委託狀態','成交口數','取消口數','條件','倉位','當沖','委託序號','委託書號','委託日期','委託時間','交易時段']),ignore_index=True)
+        print('委託:',tmplist)
+        # SKMain.ReplyCRpdMode.setdata(SKMain.replypd)
+        # SKMain.Reply_TBW.setModel(SKMain.ReplyCRpdMode)
         # i=0
         # for row in Line :
         #     print(i,',',row)
