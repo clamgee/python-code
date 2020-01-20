df94fb12b05a40e747d4d49408fea2db0681551a
clamgee <clam.ke@gmail.com>
2020-01-21 05:42:11 +0800

    1/20-1

--- demo/QThreadDemo.py
+++ demo/QThreadDemo.py
@@ -6,14 +6,13 @@ if __name__ =='__main__':
     apps=QApplication(sys.argv)
     scene=QGraphicsScene()
     plt=pg.PlotItem()
-    x = np.arange(200)
-    y1 = np.sin(x)
-    bar1=pg.BarGraphItem(x=x, height=y1, width=0.3, brush='r')
+    x = np.arange(1000)
+    y1 = np.random.randint(0,50 + 1)
+    bar1=pg.BarGraphItem(x=x, height=y1, width=1, brush='r')
     plt.addItem(bar1)
     scene.addItem(plt)
     view=QGraphicsView(scene)
     view.show()
     sys.exit(apps.exec_())
-
 # import pyqtgraph.examples
 # pyqtgraph.examples.run()
