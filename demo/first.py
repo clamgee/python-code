46d131435c45f491a40451612687443debef8050
clamgee <clam.ke@gmail.com>
2020-02-14 05:53:44 +0800

    2/12-1

--- demo/first.py
+++ demo/first.py
@@ -1,5 +1,16 @@
 import pyqtgraph as pg
-from PyQt5.QtWidgets import QApplication, QWidget
 import pandas as pd
+import numpy as np
+import sys
+
 tmp = pd.read_csv('../APIver1/result.csv')
-print(tmp.tail(5))
+tmp['ndatetime'] = pd.to_datetime(tmp['ndatetime'],format='%Y-%m-%d %H:%M:%S.%f')
+data = tmp[['close']].
+# a = pg.LinearRegionItem(data)
+pg.plot(data, pen=(255,0,0), name="Red curve")
+# pg.plot(data, title="Simplest possible plotting example")
+
+
+if __name__ == '__main__':
+    app = pg.QtGui.QApplication(sys.argv)
+    sys.exit(app.exec_())
