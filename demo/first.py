e2316597221dae7ae87dcc80aaba9701e118ce5b
clamgee <clam.ke@gmail.com>
2020-02-15 06:07:14 +0800

    2/12-1

--- demo/first.py
+++ demo/first.py
@@ -5,9 +5,25 @@ import sys
 
 tmp = pd.read_csv('../APIver1/result.csv')
 tmp['ndatetime'] = pd.to_datetime(tmp['ndatetime'],format='%Y-%m-%d %H:%M:%S.%f')
-data = tmp[['close']].
-# a = pg.LinearRegionItem(data)
-pg.plot(data, pen=(255,0,0), name="Red curve")
+data = tmp['close'].tolist()
+
+def makeKL(name):
+    vb = pg.ViewBox()
+    Plotitem = pg.PlotItem(viewBox=vb, name=name, axisItems=axisTime)
+    Plotitem.setMenuEnabled(False)
+    Plotitem.setClipToView(True)
+    Plotitem.hideAxis('left')
+    Plotitem.showAxis('right')
+    Plotitem.setRange(xRange=(0, 1), yRange=(0, 1))
+    Plotitem.getAxis('right').setWidth(60)
+    Plotitem.showGrid(x=True, y=True)
+    Plotitem.hideButtons()
+    return Plotitem
+
+
+pw = pg.PlotWidget()
+
+# pg.plot(data, pen=(255,255,0), name="Red curve")
 # pg.plot(data, title="Simplest possible plotting example")
 
 
