4b6c65602da1effaa6a60c433aaacc886e016020
clamgee <clam.ke@gmail.com>
2020-03-08 10:01:16 +0800

    3/6

--- demo/first.py
+++ demo/first.py
@@ -2,10 +2,11 @@ import pyqtgraph as pg
 import pandas as pd
 from PyQt5.QtWidgets import QMainWindow, QWidget, QGridLayout, QApplication, QGraphicsView, QGraphicsScene, QGraphicsItem, QFrame
 from PyQt5.QtCore import QRectF
+from PyQt5.QtGui import QGraphicsLayout
 from PyQt5.uic import loadUi
 import sys
 
-app = QApplication(sys.argv)
+
 tmp = pd.read_csv('../APIver1/result.csv')
 tmp['ndatetime'] = pd.to_datetime(tmp['ndatetime'], format='%Y-%m-%d %H:%M:%S.%f')
 data = tmp['close'].tolist()
@@ -14,37 +15,37 @@ dict_tmp = tmp['ndatetime'].dt.strftime('%Y/%m/%d %H:%M:%S.%f')
 dict_tmp = dict(enumerate(dict_tmp))
 # print(list(dict_tmp.keys()))
 
-lineplot = pg.PlotWidget()
-lineplot.plot(data)
-vb = pg.ViewBox()
-vb.addItem(lineplot)
-
-# class MainWindows(QMainWindow):
-#     def __init__(self):
-#         super(MainWindows, self).__init__()
-#         loadUi(r'MG.ui', self)
-#         self.lineplot = pg.PlotWidget()
-#         self.lineplot.plot(data)
-#         self.vb = pg.ViewBox()
-#         self.vb.addItem(self.lineplot)
-#         self.scene = QGraphicsScene()
-#         self.scene.addItem(self.vb)
-#         self.GV.setScene(self.scene)
-#         self.vrect = self.GV.sceneRect()
-#         self.width = self.vrect.width()
-#         self.height = self.vrect.height()
-#         self.srect = self.scene.sceneRect()
-#         print('srect:',self.srect)
-#         self.GV.mapFromScene(self.srect)
-#         # self.scene.setSceneRect(0, 0, 800, 600)
-
+class MainWindwos(QMainWindow):
+    def __init__(self):
+        super(MainWindwos, self).__init__()
+        loadUi(r'MG.ui', self)
+        self.lineplot = pg.PlotDataItem()
+        self.lineplot.setData(y=data)
+        self.vb = pg.ViewBox()
+        self.vb.addItem(self.lineplot)
+        self.scene = QGraphicsScene()
+        self.scene.addItem(self.vb)
+        self.GV.setScene(self.scene)
 
 if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    # lineplot = pg.PlotDataItem()
+    # lineplot.setData(y=data)
+    # vb = pg.ViewBox()
+    # vb.addItem(lineplot)
+    # gv = pg.GraphicsView()
+    # l = pg.Qt.QtGui.QGraphicsGridLayout()
+    # l.setHorizontalSpacing(0)
+    # l.setVerticalSpacing(0)
+    # l.addItem(vb,0,1)
+    # gv.centralWidget.setLayout(l)
+    # gv.show()
+    MW = MainWindwos()
+    MW.show()
+
+
     
-    # MW = MainWindows()
-    # print(MW.GV.geometry())
-    # MW.show()
-    vb.show()
+
     sys.exit(app.exec_())
 
 
@@ -68,13 +69,7 @@ if __name__ == '__main__':
 #         self.tmpdata = data['ndatetime'].dt.strftime('%Y/%m/%d %H:%M:%S.%f').to_dict()
 #
 #
-# class MainWindwos(QMainWindow):
-#     def __init__(self):
-#         super(MainWindwos, self).__init__()
-#         loadUi(r'MG.ui', self)
-#         self.scene = QGraphicsScene()
-#         self.scene.addItem(plot)
-#         self.GV(self.scene)
+
 #
 #
 
