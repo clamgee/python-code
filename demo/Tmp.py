45a8f4afa693a1dab09bff133f1fa4fde01b2ae5
clamgee <clam.ke@gmail.com>
2020-04-25 17:09:35 +0800

    4/24

--- demo/Tmp.py
+++ demo/Tmp.py
@@ -8,11 +8,15 @@ avgpd['low_avg'] = 0
 
 def avgline(x):
     # x = avgpd['high'].iloc[-20:].mean(0)
-    return x.iloc[-20:].mean(0)
+    if x.last_valid_index() != x.index[0]:
+        # return x.iloc[-20:].mean(0)
+        return  x.index[0]
+    else:
+        return -1
 
-#
-avgpd['high_avg'] = avgpd.high.rolling(20).mean()
-avgpd['low_avg'] = avgpd.low.rolling(20).mean()
+
+avgpd['high_avg'] = avgpd.loc[:'high'].rolling(20).mean()
+# avgpd['low_avg'] = avgpd.low.rolling(20).mean()
 
 # a = avgpd['high'].iloc[-20:].mean(0)
-print(avgpd.tail(20))
+print(avgpd.tail(25))
