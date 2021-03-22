84c6e5fa8050c80f60cb94c2946623cec4094988
clamgee <clam.ke@gmail.com>
2021-03-23 05:52:59 +0800

    20210322-1

--- demo/Tmp.py
+++ demo/Tmp.py
@@ -13,33 +13,4 @@
 #     tmp.columns[5]: 'volume',
 # }, inplace=True)
 # print(tmp.tail(5))
-import pandas as pd
-import multiprocessing as mp
-
-df = pd.DataFrame({'ser_no': [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-                'co_nm': ['aa', 'aa', 'aa', 'bb', 'bb', 'bb', 'bb', 'cc', 'cc', 'cc'],
-                'lat': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-                'lon': [21, 22, 23, 24, 25, 26, 27, 28, 29, 30]})
-
-
-
-def calc_dist(x):
-    ret =  pd.DataFrame(
-               [ [grp,
-                  df.loc[c[0]].ser_no,
-                  df.loc[c[1]].ser_no,
-                  vincenty(df.loc[c[0], x],
-                           df.loc[c[1], x])
-                 ]
-                 for grp,lst in df.groupby('co_nm').groups.items()
-                 for c in combinations(lst, 2)
-               ],
-               columns=['co_nm','machineA','machineB','distance'])
-    print(ret)
-    return ret
-
-if __name__ == '__main__':
-    pool = mp.Pool(processes = (mp.cpu_count() - 1))
-    pool.map(calc_dist, ['lat','lon'])
-    pool.close()
-    pool.join()
+import pandarallel as pdl
