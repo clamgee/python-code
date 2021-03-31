4e6c87983a37975e2fdf5c03905978932e6ede16
clamgee <clam.ke@gmail.com>
2021-03-31 17:12:51 +0800

    20210331

--- demo/Tmp.py
+++ demo/Tmp.py
@@ -1,16 +1,17 @@
-# import pandas as pd
-# import os
+import pandas as pd
+import os
 
-# domain=os.listdir('../APIver1/data')
-# print(domain[-1])
-# tmp=pd.read_csv('../APIver1/data/'+domain[-1])
-# tmp.rename(columns={
-#     tmp.columns[0]: 'ndate',
-#     tmp.columns[1]: 'ntime',
-#     tmp.columns[2]: 'nbid',
-#     tmp.columns[3]: 'nask',
-#     tmp.columns[4]: 'close',
-#     tmp.columns[5]: 'volume',
-# }, inplace=True)
+domain=os.listdir('../data/')
+print(domain[-1])
+tmp=pd.read_csv('../data/'+domain[-1])
+tmp.rename(columns={
+    tmp.columns[0]: 'ndate',
+    tmp.columns[1]: 'ntime',
+    tmp.columns[2]: 'nbid',
+    tmp.columns[3]: 'nask',
+    tmp.columns[4]: 'close',
+    tmp.columns[5]: 'volume',
+}, inplace=True)
 # print(tmp.tail(5))
-print(list('abc'))
+
+print(tmp.volume.cumsum())
