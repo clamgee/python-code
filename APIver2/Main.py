b5ab86a55541b8864cb24970a3cdddb12945c8d7
clamgee <clam.ke@gmail.com>
2021-05-14 06:16:59 +0800

    20210513-4

--- APIver2/Main.py
+++ APIver2/Main.py
@@ -621,11 +621,12 @@ class SKQuoteLibEvents:
     def OnConnection(self, nKind, nCode):
         if (nKind == 3001):
             strMsg = "Connected!"
-            SKMain.commodityFnc()
         elif (nKind == 3002):
             strMsg = "DisConnected!"
         elif (nKind == 3003):
             strMsg = "Stocks ready!"
+            time.sleep(5)
+            SKMain.commodityFnc()
         elif (nKind == 3021):
             strMsg = "Connect Error!"
         print(strMsg)
