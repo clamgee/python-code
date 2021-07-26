53b880938397da57a0ee5540061c66c6d40fbfbe
clamgee <clam.ke@gmail.com>
2021-07-27 06:07:47 +0800

    20210726-1

--- demo/Tmp.py
+++ demo/Tmp.py
@@ -1,57 +1,46 @@
-import multiprocessing as mp
-from multiprocessing.process import current_process
-import threading as td
-import os
-import time
-
-def a(*args):
-    while args[0].qsize!=0:
-        with args[1]:
-            a=args[0].get()
-            b=os.getpid()
-            print(a,str(b)+'a程式, 程序: ',current_process().name)
-
-def b(*args):
-    td.Thread(target=args[0],args=(args[1],args[2])).start()
-
-def c(*args):
-    while True:
-        args[1].acquire()
-        a=args[0].get()
-        b=os.getpid()
-        print(a,str(b)+'c程式, 程序: ',current_process().name)
-        args[1].release()
-
-def d(*args):
-    td.Thread(target=args[0],args=(args[1],args[2])).start()
-
-def Pwork(func,*args):
-    start = time.time()
-    p1=mp.Process(target=func,args=(args[0],args[1],args[2],))
-    p1.start()
-    p1.join()
-    end = time.time()
-    print('process時間: ',end - start)
-
-def multiwork(func,*args):
-    start = time.time()
-    with mp.Pool() as pool:
-        pool.apply(func,(args[0],args[1],args[2],))
-    end = time.time()
-    print('pool時間: ',end - start)
+from multiprocessing import Process, Queue, current_process
+# from PyQt5 import QThread,pyqtSignal,QMainWindow
+from PyQt5.QtCore import pyqtSlot, QDate, QTime, QDateTime, QTimer, Qt, QThread, pyqtSignal, \
+    QAbstractTableModel  # 插入資訊模組
+from PyQt5.QtWidgets import QApplication, QDialog, QFileDialog, QMainWindow, QGraphicsScene, QHeaderView, \
+    QTableWidgetItem, QMessageBox  # PyQt5介面與繪圖模組
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
+class communicate_Thread(QThread):
 
-if __name__=='__main__':
-    # pool = mp.Pool()
-    l=mp.Lock()
-    l2=mp.Manager().RLock()
-    q=mp.Queue()
-    q2=mp.Manager().Queue()
-    for i in range(10):
-        Astr='第 '+str(i)+' 步'
-        q.put(Astr)
-        q2.put(Astr)
-    Pwork(b,a,q,l)
-    multiwork(d,c,q2,l2)
+    communicate_singal = pyqtSignal(str)
+    exit_signal = pyqtSignal()
 
+    def __init__(self, q, parent=None,):
+        QThread.__init__(self, parent)
+        self.q = q
+
+    def run(self):
+        # global running
+        while True:
+            text = self.q.get(True)
+            self.communicate_singal.emit(text)
+
+        self.exit_signal.emit()
+
+class App(QMainWindow):
+    def __init__(self):
+        self.q = Queue()
+        self.xxx = communicate_Thread(self.q)
+        self.xxx.communicate_singal.connect(self.do_something)
+        self.xxx.start()
+        
+        self.yyy_process = Process(target=self.yyyprocess, args=(self.q,))
+        self.yyy_process.start()
+        
+    def yyyprocess(self,q):
+        res = []
+        i = 0
+        a = current_process().name
+        for i in range(10):
+            res.append([i,a])
+            q.put(res)
+
+if __name__ == '__main__':
+    QMain = App()
--- demo/multi.py
+++ demo/multi.py
@@ -33,51 +33,64 @@ if __name__=='__main__':
 
 #############方法二
 import multiprocessing as mp
+from multiprocessing.process import current_process
 import threading as td
 import os
 import time
 
 def a(*args):
-    while True:
+    while args[0].qsize>0:
         with args[1]:
             a=args[0].get()
             b=os.getpid()
-            print(a,str(b)+'a程式')
+            print(a,str(b)+'a程式, 程序: ',current_process().name)
 
 def b(*args):
-    td.Thread(target=args[0],args=(args[1],args[2])).start()
+    t1=td.Thread(target=args[0],args=(args[1],args[2]))
+    t1.start()
 
 def c(*args):
     while True:
         args[1].acquire()
         a=args[0].get()
         b=os.getpid()
-        print(a,str(b)+'c程式')
+        print(a,str(b)+'c程式, 程序: ',current_process().name)
         args[1].release()
 
 def d(*args):
-    td.Thread(target=args[0],args=(args[1],args[2])).start()
+    t1=td.Thread(target=args[0],args=(args[1],args[2]))
+    t1.start()
+
+
+def Pwork(func,*args):
+    start = time.time()
+    p1=mp.Process(target=func,args=(args[0],args[1],args[2],))
+    p1.start()
+    p1.join()
+    end = time.time()
+    print('process時間: ',end - start)
 
 def multiwork(func,*args):
-    pool.apply(func,(args[0],args[1],args[2],))
+    start = time.time()
+    with mp.Pool() as pool:
+        pool.apply(func,(args[0],args[1],args[2],))
+    end = time.time()
+    print('pool時間: ',end - start)
 
 
 
 if __name__=='__main__':
-    pool = mp.Pool()
-    l=mp.Manager().Lock()
-    l2=mp.Manager().Lock()
-    q=mp.Manager().Queue()
+    # pool = mp.Pool()
+    l=mp.Lock()
+    l2=mp.Manager().RLock()
+    q=mp.Queue()
     q2=mp.Manager().Queue()
-    multiwork(b,a,q,l)
-    multiwork(d,c,q2,l2)
-    pool.close()
     for i in range(10):
         Astr='第 '+str(i)+' 步'
         q.put(Astr)
         q2.put(Astr)
-    pool.join()
-
+    # multiwork(d,c,q2,l2)
+    Pwork(b,a,q,l)
 # ==========第三種
 import multiprocessing as mp
 from multiprocessing.process import current_process
@@ -140,3 +153,92 @@ if __name__=='__main__':
     print('Pool時間: ',time.time()-start)
 
 
+# ====== 各種比較
+import multiprocessing as mp
+from multiprocessing import current_process
+import threading as td
+import time
+
+
+def funcq(q,mq,asyncq):
+    for i in range(30000):
+        res = i+i**2
+        q.put(res)
+        mq.put(res)
+        asyncq.put(res)
+
+def funcgetp(*args):
+    res=[]
+    while args[0].qsize()>0:
+        with args[1]:
+            a=args[0].get()
+            c=current_process().name
+            res.append([a,c])
+            # print('Process:',a,c)
+    print('Process: ',res[8],', len: ',len(res))
+    
+
+
+def funcgetm(*args):
+    res=[]
+    # print('Multi Queue: ',args[0].qsize())
+    while args[0].qsize()>0:
+        # args[1].acquire()
+        a=args[0].get()
+        c=current_process().name
+        res.append([a,c])
+        # print('Multi: ',a,c)
+        # args[1].release()
+    print('Multi: ',res[8],', len: ',len(res))  
+
+def funcgetasync(*args):
+    a=[args[0],current_process().name]
+    return a
+
+
+def functd(*args):
+    # print('thread',args[0],args[1])
+    t1=td.Thread(target=args[0],args=(args[1],args[2],))
+    t1.start()
+
+def Pwork(func,*args):
+    # print('work',len(args),args[0],args[1])
+    start = time.time()
+    w1 = mp.Process(target=func,args=(args[0],args[1],args[2],))
+    w1.start()
+    w1.join()
+    print('Process',time.time()-start)
+
+def Multiwork(func,*args):
+    # print('work',len(args),args[0],args[1])
+    start=time.time()
+    pool=mp.Pool()
+    pool.apply(func,(args[0],args[1],args[2],))
+    pool.close()
+    pool.join()
+    print('Mulit',time.time()-start)
+
+def Asyncwork(func,*args):
+    pool=mp.Pool()
+    res=[]
+    start=time.time()
+    while args[0].qsize()>0:
+        a = args[0].get()
+        res.append(pool.apply_async(func,(a,)))
+    pool.close()    
+    pool.join()
+    print('Async: ',res[8],', len: ',len(res))
+    print('Async',time.time()-start)
+
+
+if __name__=='__main__':
+    q=mp.Queue()
+    l=mp.Lock()
+    mq=mp.Manager().Queue()
+    ml=mp.Manager().RLock()
+    asyncq=mp.Manager().Queue()
+    funcq(q,mq,asyncq)
+    Pwork(functd,funcgetp,q,l)
+    Multiwork(functd,funcgetm,mq,ml)
+    Asyncwork(funcgetasync,asyncq)
+    
