3a677b545a2fe32dc08ee45f4bd914b879713f5b
clamgee <clam.ke@gmail.com>
2021-08-06 06:03:01 +0800

    20210805-1

--- demo/Tmp.py
+++ demo/Tmp.py
@@ -1,46 +1,25 @@
 import sys
-from PySide6 import QtCore,QtWidgets,QtGui
 from PySide6.QtUiTools import QUiLoader
-
-from wigglywidget import WigglyWidget
-
-import QtDesigner
-
-
-TOOLTIP = "A cool wiggly widget (Python)"
-DOM_XML = """
-<ui language='c++'>
-    <widget class='WigglyWidget' name='wigglyWidget'>
-        <property name='geometry'>
-            <rect>
-                <x>0</x>
-                <y>0</y>
-                <width>400</width>
-                <height>200</height>
-            </rect>
-        </property>
-        <property name='text'>
-            <string>Hello, world</string>
-        </property>
-    </widget>
-</ui>
-"""
-
-QtDesigner.QPyDesignerCustomWidgetCollection.registerCustomWidget(WigglyWidget, module="wigglywidget",tool_tip=TOOLTIP, xml=DOM_XML)
-
-class SKMainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(SKMainWindow, self).__init__()
-        self.loadUi = QUiLoader()
-        self.loadUi.load(r'../APIver3/UI/MainWindow.ui')
-
-
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QFile, QIODevice
+import os
+envpath = r'C:/Users/Owner/AppData/Local/Programs/Python/Python37/Lib/site-packages/PySide6/plugins/designer/PySidePlugin.dll'
+os.environ["PYSIDE_DESIGNER_PLUGINS"] = envpath
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-    SKMain = SKMainWindow()
-    # SKMain.showMaximized()
-    SKMain.show()
+    app = QApplication(sys.argv)
 
+    ui_file_name = "../APIver3/UI/MainWindow.ui"
+    ui_file = QFile(ui_file_name)
+    if not ui_file.open(QIODevice.ReadOnly):
+        print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
+        sys.exit(-1)
+    loader = QUiLoader()
+    window = loader.load(ui_file)
+    ui_file.close()
+    if not window:
+        print(loader.errorString())
+        sys.exit(-1)
+    window.show()
 
     sys.exit(app.exec())
