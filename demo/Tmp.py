8b7d5c1afb5e05d8fbf5c12d2b9b9dc7211c64aa
clamgee <clam.ke@gmail.com>
2021-08-12 05:52:32 +0800

    20210811-1

--- demo/Tmp.py
+++ demo/Tmp.py
@@ -1,4 +1,26 @@
 
+from sys import platform
+
+import winreg
+from winreg import HKEY_LOCAL_MACHINE
+import platform
+print(platform.machine())
+
+def open_key(key):
+    """Open the register key.
+    Args:
+        key (str): The key of register.
+    Returns:
+        str: The handle to the specified key.
+    """
+    machine_type = platform.machine()
+    mappings = {"AMD64": winreg.KEY_WOW64_64KEY}
+    return winreg.OpenKey(
+        winreg.HKEY_LOCAL_MACHINE,
+        key,
+        access=winreg.KEY_READ | mappings.get(machine_type, winreg.KEY_WOW64_32KEY),
+    )
+
 # import os
 # import PySide2
 # dirname = os.path.dirname(PySide2.__file__)
@@ -7,41 +29,41 @@
 # print(plugin_path)
 
 
-import sys
-import os
-import PySide6
-from PySide6.QtWidgets import QApplication, QMainWindow
-from PySide6.QtCore import QFile
-from PySide6.QtUiTools import QUiLoader
-from MainWindows import Ui_CapitalAPI
-os.environ['PYSIDE_DESIGNER_PLUGINS'] = "C:\\Users\\Gary\\anaconda3\\Lib\\site-packages\\PySide6\\plugins"
-# dirname = os.path.dirname(PySide6.__file__)
-# plugin_path = os.path.join(dirname, 'plugins')
-# os.environ['PYSIDE_DESIGNER_PLUGINS'] = plugin_path
-# print(plugin_path)
+# import sys
+# import os
+# import PySide6
+# from PySide6.QtWidgets import QApplication, QMainWindow
+# from PySide6.QtCore import QFile
+# from PySide6.QtUiTools import QUiLoader
+# # from MainWindows import Ui_CapitalAPI
+# # os.environ['PYSIDE_DESIGNER_PLUGINS'] = "C:\\Users\\Gary\\anaconda3\\Lib\\site-packages\\PySide6\\plugins"
+# # dirname = os.path.dirname(PySide6.__file__)
+# # plugin_path = os.path.join(dirname, 'plugins')
+# # os.environ['PYSIDE_DESIGNER_PLUGINS'] = plugin_path
+# # print(plugin_path)
 
 
-class MyMainWindow(QMainWindow):
-    def __init__(self):
-        super(MyMainWindow, self).__init__()
-        self.load_ui()
+# class MyMainWindow(QMainWindow):
+#     def __init__(self):
+#         super(MyMainWindow, self).__init__()
+#         self.load_ui()
     
-    def load_ui(self):
-        self.ui = Ui_CapitalAPI()
-        self.ui.setupUI(self)
+#     # def load_ui(self):
+#     #     self.ui = Ui_CapitalAPI()
+#     #     self.ui.setupUI(self)
 
-    # def load_ui(self):
-    #     loader = QUiLoader()
-    #     path = os.path.join(os.path.dirname(__file__), "../APIver3/UI/MainWindow.ui")
-    #     ui_file = QFile(path)
-    #     ui_file.open(QFile.ReadOnly)
-    #     loader.load(ui_file, self)
-    #     # ui_file.close()
+#     def load_ui(self):
+#         loader = QUiLoader()
+#         path = os.path.join(os.path.dirname(__file__), "../APIver3/UI/MainWindow.ui")
+#         ui_file = QFile(path)
+#         ui_file.open(QFile.ReadOnly)
+#         loader.load(ui_file, self)
+#         ui_file.close()
 
-if __name__ == "__main__":
-    app = QApplication([])
-    widget = MyMainWindow()
-    widget.show()
-    sys.exit(app.exec_())
+# if __name__ == "__main__":
+#     app = QApplication([])
+#     widget = MyMainWindow()
+#     widget.show()
+#     sys.exit(app.exec())
 
 
