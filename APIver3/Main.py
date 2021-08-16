1690ab856fcdec514c02267b548865d47baf66be
clamgee <clam.ke@gmail.com>
2021-08-17 06:06:37 +0800

    20210816-4

--- APIver3/Main.py
+++ APIver3/Main.py
@@ -1,37 +1,31 @@
 import sys
 import os
-from PySide6.QtCore import Signal,Slot,QTime,Qt,QThread,QAbstractTableModel,QFile
+from PySide6.QtCore import QObject, Signal,Slot,QTime,Qt,QThread,QAbstractTableModel,QFile
 from PySide6.QtWidgets import QMainWindow,QApplication,QTableWidgetItem,QMessageBox,QHeaderView
 from PySide6.QtGui import QAction
 import pyqtgraph as pg
-from UI.Uiload import UiLoader
-from UI.ui_MainWindow import Ui_CapitalAPI
+from PySide6.QtUiTools import QUiLoader
+# from UI.Uiload import UiLoader
+# from UI.ui_MainWindow import Ui_CapitalAPI
 # 程式路徑
 SCRIPT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 # print(os.path.join(SCRIPT_DIRECTORY, 'UI\MainWindow.ui'))
 
-class SKMainWindow(QMainWindow):
-    def __init__(self, parent=None):
-        QMainWindow.__init__(self, parent)
-<<<<<<< HEAD
-        # self.MainUi=Ui_CapitalAPI()
-        # self.MainUi.setupUi(self)
-        self.MainWindowUiFile = QFile(os.path.join(SCRIPT_DIRECTORY, 'UI\MainWindow.ui'))
+class SKMainWindow(QObject):
+    def __init__(self,MainUIFile,parent=None):
+        super(SKMainWindow, self).__init__(parent)
+        # QMainWindow.__init__(self, parent)
+        self.MainWindowUiFile = QFile(os.path.join(SCRIPT_DIRECTORY, MainUIFile))
         self.MainWindowUiFile.open(QFile.ReadOnly)
-        self.MainUi=UiLoader(self.MainWindowUiFile, self)
+        self.Loader=QUiLoader()
+        self.MainUi=self.Loader.load(self.MainWindowUiFile)
+        # self.Loader.load('UI\MainWindow.ui')
         self.MainWindowUiFile.close()
-=======
-        self.MainUi=Ui_CapitalAPI()
-        self.MainUi.setupUi(self)
-        # self.MainWindowUiFile = QFile(os.path.join(SCRIPT_DIRECTORY, 'UI\MainWindow.ui'))
-        # # self.MainWindowUiFile.open(QFile.ReadOnly)
-        # UiLoader(self.MainWindowUiFile, self)
-        # # self.MainWindowUiFile.close()
->>>>>>> ffbbc14eedf0cd1c5207b2ec2e44895d7b62ff5e
+        self.MainUi.show()
+        self.MainUi.showMaximized()
 
 
 if __name__=='__main__':
     SKApp = QApplication(sys.argv)
-    SKMain = SKMainWindow()
-    SKMain.show()
+    SKMain = SKMainWindow('UI\MainWindow.ui')
     sys.exit(SKApp.exec_())
