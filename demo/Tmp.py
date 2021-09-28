2c9dfa5264b3e03633b23acfb623908b32d52bc2
clamgee <clam.ke@gmail.com>
2021-09-29 06:06:21 +0800

    20210928-1

--- demo/Tmp.py
+++ demo/Tmp.py
@@ -1,127 +1,81 @@
-import multiprocessing as mp
-from multiprocessing import current_process, set_executable, Manager
-import PySide6
-from PySide6.QtCore import QObject, QThread, Signal,Slot
-from PySide6.QtWidgets import QWidget
+from multiprocessing import Process, Queue, Pipe
 from threading import Thread
-from PySide6.QtWidgets import QApplication
-import time,os
-from pyqtgraph import GraphicsLayoutWidget
-import pyqtgraph as pg
-import pandas as pd
-import numpy as np
-from multiprocessing import *
-import multiprocessing.sharedctypes as sharedctypes
-import dill
-import ctypes
-# global pass1_signal,pass_signal
-# pass_signal = Signal(list)
-# pass1_signal = Signal(str)
 
-class QSignal(QObject):
-    pass_signal = Signal(list)
+from PySide6 import QtGui, QtCore, QtWidgets
 
-class Q(QWidget): 
-    def __init__(self,parent):
-        self.Qdata = mp.Queue()
-        self.pass_signal = parent.pass_signal
-        self.pass_signal.connect(self.receive)
+class Emitter(QtCore.QObject, Thread):
+    def __init__(self, transport, parent=None):
+        QtCore.QObject.__init__(self, parent)
+        Thread.__init__(self)
+        self.transport = transport
 
-    def receive(self,nlist):
-        print(nlist)
-
-class Testthread(QThread):
-    def __init__(self,*args):
-        super(Testthread,self).__init__()
-        self.name = args[0]
-        self.idx = args[1]
-        self.msgtuple = args[2]
-        # self.msgtuple = dill.loads(args[2])
-        self.Func = 0
-        # print(C.__getattribute__(C.pass_signal))
-
-    def func(self):
-        self.Func = self.Func + self.idx
-        print(self.Func)
+    def _emit(self, signature, args=None):
+        if args:
+            self.emit(QtCore.SIGNAL(signature), args)
+        else:
+            self.emit(QtCore.SIGNAL(*signature))
 
     def run(self):
-            self.func()
-            self.msgtuple.emit([1,2])
-            time.sleep(2)
+        while True:
+            try:
+                signature = self.transport.recv()
+            except EOFError:
+                break
+            else:
+                self._emit(*signature)
 
-class MyProcess(mp.Process):  # 定义一个类，继承Process类
-    def __init__(self,func,*args):
-        super(MyProcess, self).__init__()  # 实现父类的初始化方法
-        self.name1 = args[0]
-        self.idx = args[1]
-        self.msgtuple = args[2]
-        self.target = func
+class Form(QtWidgets.QDialog):
+    def __init__(self, queue, emitter, parent=None):
+        super().__init__(parent)
+        self.data_to_child = queue
+        self.emitter = emitter
+        self.emitter.daemon = True
+        self.emitter.start()
+        self.browser = QtWidgets.QTextBrowser()
+        self.lineedit = QtWidgets.QLineEdit('Type text and press <Enter>')
+        self.lineedit.selectAll()
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.browser)
+        layout.addWidget(self.lineedit)
+        self.setLayout(layout)
+        self.lineedit.setFocus()
+        self.setWindowTitle('Upper')
+        self.lineedit.returnPressed.connect(self.to_child)
+        self.connect(self.emitter, QtCore.SIGNAL('data(PyObject)'), self.updateUI)
 
-    def run(self):  # 必须实现的方法，是启动进程的方法
-        TD = self.target(self.name1,self.idx,self.msgtuple)
-        TD.run()
-        nowproc=mp.current_process()
-        print('子进程:', os.getpid(), os.getppid(),nowproc)
+    def to_child(self):
+        self.data_to_child.put(self.lineedit.text())
+        self.lineedit.clear()
 
+    def updateUI(self, text):
+        print(text)
+        self.browser.append(text[0])
 
-class AClass:
-    def __init__(self,inputsignal):
-        self.name ='A'
-        self.index = 1
-        # self.msgtuple = signal
-        self.A = MyProcess(Testthread,self.name,self.index,inputsignal)
-        self.A.start()
+class ChildProc(Process):
+    def __init__(self, transport, queue, daemon=True):
+        Process.__init__(self)
+        self.daemon = daemon
+        self.transport = transport
+        self.data_from_mother = queue
 
-        # self.B = MyProcess(self.creattd,'B',0,self.msgtuple)
-        # self.B.start()
-   
-    def creattd(self,func,*args):
-        print(args[0],args[1],)
-        TD = func(args[0],args[1])
-        TD.run()
-        print(TD.currentThread())
+    def emit_to_mother(self, signature, args=None):
+        signature = (signature, )
+        if args:
+            signature += (args, )
+        self.transport.send(signature)
 
-def creattd(*args):
-    print(args[0],args[1],args[2])
-    TD = Testthread(args[0],args[1],args[2])
-    TD.start()
-    print(TD.currentThread())
+    def run(self):
+        while True:
+            text = self.data_from_mother.get()
+            self.emit_to_mother('data(PyQt_PyObject)', (text.upper(),))
 
-
-if __name__ =='__main__':
-    mp.set_start_method('spawn')
-    app = QApplication()
-    # B = MyProcess(creattd,'A',1,(1,2))
-    # B.start()
-    Sig = QSignal()
-    global C
-    C = Q(parent=Sig)
-    # psgl = dill.dumps(C)
-    # QT = Testthread('A',1)
-    # QT.start()
-    B = AClass(Sig.pass_signal)
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    mother_pipe, child_pipe = Pipe()
+    queue = Queue()
+    emitter = Emitter(mother_pipe)
+    form = Form(queue, emitter)
+    ChildProc(child_pipe, queue).start()
+    form.show()
     app.exec_()
-
-
-# import sys
-# from PySide6 import QtCore,QtWidgets
-# import typing
-# # define a new slot that receives a string and has
-# # 'saySomeWords' as its name
-# class Communicate(QtCore.QObject):
-#     speak = QtCore.Signal(str)
-#     def __init__(self):
-#         super(Communicate, self).__init__()
-#         self.ButtonA = None
-#         print('in class %s',self.ButtonA)
-#         self.speak.connect(self.saySomeWords)
-#     @QtCore.Slot(str)
-#     def saySomeWords(self,words):
-#         print(words)
-
-# if __name__=='__main__':
-#     someone = Communicate()
-#     # connect signal and slot
-#     # someone.speak.connect(saySomeWords)
-#     # emit 'speak' signal
-#     someone.speak.emit("Hello everybody!")
