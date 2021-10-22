e307ea56e64e13d93a9ab3bdc875c59f2d4b5b3f
clamgee <clam.ke@gmail.com>
2021-10-23 06:05:34 +0800

    20211022-2

--- demo/Tmp.py
+++ demo/Tmp.py
@@ -1,3 +1,4 @@
+# %%
 import pandas as pd
 import numpy as np
 import multiprocessing as mp
@@ -8,11 +9,8 @@ from PySide6.QtWidgets import QTableWidgetItem
 from PySide6.QtGui import QFont
 print(time.localtime().tm_hour)
 
-# Candledf=pd.read_csv('../result.dat',low_memory=False)
-# Candledf['ndatetime']=pd.to_datetime(Candledf['ndatetime'],format='%Y-%m-%d %H:%M:%S.%f')
+Candledf=pd.read_csv('../result.dat',low_memory=False)
+Candledf['ndatetime']=pd.to_datetime(Candledf['ndatetime'],format='%Y-%m-%d %H:%M:%S.%f')
 
-a = pg.GraphicsObject()
-a._updateView()
-b=pg.plot()
-b.addItem(a)
-b.updateMatrix(propagate=True)
+
+# %%
