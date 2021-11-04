7a6729e2474bc346852a969767e455963e2f463f
clamgee <clam.ke@gmail.com>
2021-11-04 22:24:58 +0800

    20211104

--- demo/Tmp.py
+++ demo/Tmp.py
@@ -1,12 +1,17 @@
-import numpy as np
-import pandas as pd
-# import tensorflow as tf
-# import matplotlib.pyplot as plt
-import seaborn as sns
-from PySide6.QtCore import QThread
-import os
-a = QThread()
-os.getppid()
+import multiprocessing as mp
+import timeit
+import time
+_event = mp.Event()
+
+def func():
+    for i in range(10):
+        if _event.is_set() is False:
+            _event.set()
+        _event.wait()
+        # time.sleep(0.0001)
+        _event.clear()
+t = timeit.timeit(func,number=100)
+print(t)
 
 # sns.set()
 # Candledf=pd.read_csv('../result.dat',low_memory=False)
