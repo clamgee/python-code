4a8bdf44b9f51e5e9d35fc58308960b3ca7dd074
clamgee <clam.ke@gmail.com>
2021-11-09 06:06:42 +0800

    20211108-1

--- demo/Tmp.py
+++ demo/Tmp.py
@@ -1,6 +1,8 @@
-from PySide6.QtCore import QThread
-
-
+import numpy as np
+timesteps=8; data_dim=16
+x_train = np.random.random((10, timesteps, data_dim))
+print(x_train.shape)
+print(x_train[-1])
 # sns.set()
 # Candledf=pd.read_csv('../result.dat',low_memory=False)
 # Candledf['ndatetime']=pd.to_datetime(Candledf['ndatetime'],format='%Y-%m-%d %H:%M:%S.%f')
