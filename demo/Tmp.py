e2ed6854bc03560d449d3abf8d82d1dce5d1b8a7
clamgee <clam.ke@gmail.com>
2021-11-29 06:06:42 +0800

    20211126-5

--- demo/Tmp.py
+++ demo/Tmp.py
@@ -1,16 +1,9 @@
 
-
 import numpy as np
 import pandas as pd
-# lottorydf = pd.read_csv('lottory.csv')
-# lottorydf=lottorydf.sort_values(by=['期號'],ascending=True)
-# lottorydf=lottorydf.reset_index(drop=True)
-# print(lottorydf.tail())
-# lottorydf.map
-# sns.set()
-# Candledf=pd.read_csv('../result.dat',low_memory=False)
-# Candledf['ndatetime']=pd.to_datetime(Candledf['ndatetime'],format='%Y-%m-%d %H:%M:%S.%f')
 
-
-a = pd.DataFrame()
-print(a.last_valid_index())
+Candledf=pd.read_csv('../result.dat',low_memory=False)
+Candledf['ndatetime']=pd.to_datetime(Candledf['ndatetime'],format='%Y-%m-%d %H:%M:%S.%f')
+laxtidx = Candledf.last_valid_index()
+print(Candledf.at[laxtidx,'ndatetime'].time().strftime('%H:%M:%S.%f'))
+print(type(Candledf.at[laxtidx,'ndatetime'].time().strftime('%H:%M:%S.%f')))
