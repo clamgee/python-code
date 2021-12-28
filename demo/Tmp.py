3bad558c335f53b8d147c8cc9b3a368d5cd39b5b
clamgee <clam.ke@gmail.com>
2021-12-29 05:45:06 +0800

    20211228-1

--- demo/Tmp.py
+++ demo/Tmp.py
@@ -3,9 +3,10 @@ import numpy as np
 import pandas as pd
 import os
 import datetime
-a = [1,3,4]
-b = pd.Series([0]*3+a)
-print(b)
+import APIver3.Config_dict as Config_dict
+
+print(Config_dict['BuySell'])
+
 # Candledf=pd.read_csv('../result.dat',low_memory=False)
 # Candledf['ndatetime']=pd.to_datetime(Candledf['ndatetime'],format='%Y-%m-%d %H:%M:%S.%f')
 # laxtidx = Candledf.last_valid_index()
